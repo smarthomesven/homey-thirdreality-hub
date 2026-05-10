4aff1451ef3f59ee0d5728f4b5d18ab5fd830ab3
smarthomesven <sven@dypodex.com>
2026-05-11 01:25:44 +0200

    Added repair view

--- drivers/plug/driver.js
+++ drivers/plug/driver.js
@@ -14,6 +14,13 @@ module.exports = class MyDriver extends Homey.Driver {
 
   async onPair(session) {
     this.log('MyDriver pairing has started');
+    session.setHandler("showView", async (viewId) => {
+      if (viewId === 'login') {
+        const loggedIn = this.homey.settings.get('loggedIn');
+        return loggedIn === true;
+      }
+      return false;
+    });
     session.setHandler("login", async (data) => {
       try {
         const user = data.email;
@@ -53,6 +60,43 @@ module.exports = class MyDriver extends Homey.Driver {
         return await this.onPairListDevices();
       } catch (error) {
         throw new Error("Error while fetching devices: " + error.message);
+      }
+    });
+  }
+
+  async onRepair(session) {
+    this.log('MyDriver repairing has started');
+    session.setHandler("login", async (data) => {
+      try {
+        const user = data.email;
+        const password = data.password;
+        if (!data.email || !data.password) {
+          return false;
+        }
+        const response = await axios.post('https://cloud.3reality.com/realitycloudserver/user/login', {
+          data: {
+            requester: "APP",
+            account: user,
+            password: password
+          },
+          version: "V1.0.0"
+        });
+        if (!response.data.result.data.token || !response.data.result.data.userId) {
+          return false;
+        }
+        this.homey.settings.set('user', user);
+        this.homey.settings.set('password', password);
+        this.homey.settings.set('loggedIn', true);
+        this.homey.settings.set('token', response.data.result.data.token);
+        this.homey.settings.set('userId', response.data.result.data.userId);
+        await this.homey.app.connectIfLoggedIn();
+        await session.done();
+        return true;
+      } catch (error) {
+        if (error.response && error.response.status === 401) {
+          return false;
+        }
+        throw new Error("Error during login check: " + error.message);
       }
     });
   }
